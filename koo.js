0a73d05ca73a20f01dac5ae7f1a18d53ca0d5ae1
Ajay Kushwaha <ajayk96442568@gmail.com>
2023-07-26 11:25:04 +0530

    Added koo-clone project to repository

---
+++ koo.js
@@ -0,0 +1,9 @@
+let navbarcode = document.getElementById("qr-img");
+
+navbarcode.addEventListener('mouseover',function(){
+    card.style.cssText = "display:flex";
+})
+navbarcode.addEventListener('mouseleave',function(){
+    card.style.cssText = "display:none";
+})
+
